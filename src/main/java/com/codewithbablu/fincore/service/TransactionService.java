a26a6d6a2e8f563e97bd14540862d27967f8b060
bablu kumar <bablu707041dk@gmail.com>
2026-01-14 17:35:35 +0530

    feat: implement async transaction processing service

---
+++ src/main/java/com/codewithbablu/fincore/service/TransactionService.java
@@ -0,0 +1,30 @@
+package com.codewithbablu.fincore.service;
+
+import com.codewithbablu.fincore.model.Transaction;
+import org.springframework.stereotype.Service;
+import java.util.concurrent.ExecutorService;
+
+public class TransactionService {
+
+    private final ExecutorService taskExecutor;
+
+    public TransactionService(ExecutorService taskExecutor) {
+        this.taskExecutor = taskExecutor;
+    }
+
+    public Transaction createTransaction(double amount, String type){
+        Transaction txn = new Transaction(amount, type);
+
+        // Logic 2 : Async Processing (Fire and Forget)
+        taskExecutor.submit(() ->{
+            System.out.println("Processing " + txn.id() + " On Thread "+Thread.currentThread());
+            try{
+                Thread.sleep(1000);
+                System.out.println("Completed " + txn.id());
+            }catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
+        });
+        return txn;
+    }
+}
