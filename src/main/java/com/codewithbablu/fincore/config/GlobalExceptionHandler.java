3cb52aaa0f795a58e158d5134fde5f7f3ee18229
bablu kumar <bablu707041dk@gmail.com>
2026-01-14 19:19:48 +0530

    feat: add global exception handler for clean error responses

---
+++ src/main/java/com/codewithbablu/fincore/config/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.codewithbablu.fincore.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleBadInput(IllegalArgumentException e){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        "error",e.getMessage(),
+                        "timestamp",LocalDateTime.now(),
+                        "status", 400
+                ));
+
+    }
+ }
