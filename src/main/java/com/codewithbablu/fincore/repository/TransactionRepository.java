79ee131be04c0a275c501c4d140cacd2a384cde9
bablu kumar <bablu707041dk@gmail.com>
2026-01-15 21:53:34 +0530

    feat: aaded the TransactionRepositry

---
+++ src/main/java/com/codewithbablu/fincore/repository/TransactionRepository.java
@@ -0,0 +1,35 @@
+package com.codewithbablu.fincore.repository;
+
+
+import com.codewithbablu.fincore.model.Transaction;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class TransactionRepository {
+    // "Beast" Logic : Thread-Safe Memory Storage
+    // Key = Transaction ID (String), Value = Transaction Object
+    private final ConcurrentHashMap<String, Transaction> transactionStore = new ConcurrentHashMap<>();
+
+    // 1. Save Data
+    public Transaction save(Transaction transaction){
+        transactionStore.put(transaction.id(),transaction);
+        return transaction;
+    }
+
+    // 2. Get All Data (Values ko list Me Converte karke return karenge)
+    public List<Transaction> findAll(){
+        return new ArrayList<>(transactionStore.values());
+    }
+
+    // 3. Find by ID (Optional return karenge taki NullPointerException na aaye)
+    // Optional Java 8+ ka feature hai. ye box jaisa hai ye to item hoga, ya empty hoga
+    public Optional<Transaction> findById(String id) {
+    return Optional.ofNullable(transactionStore.get(id));
+    }
+
+}
