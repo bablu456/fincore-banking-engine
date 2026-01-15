51544a81dcad624d2ae00f0d121e988290cfb817
bablu kumar <bablu707041dk@gmail.com>
2026-01-15 22:01:06 +0530

    Updated The TransactionService

--- src/main/java/com/codewithbablu/fincore/service/TransactionService.java
+++ src/main/java/com/codewithbablu/fincore/service/TransactionService.java
@@ -1,15 +1,20 @@
 package com.codewithbablu.fincore.service;
 
 import com.codewithbablu.fincore.model.Transaction;
+import com.codewithbablu.fincore.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 @Service
 public class TransactionService {
 
     private final ExecutorService taskExecutor;
+    private final TransactionRepository repository;
 
-    public TransactionService(ExecutorService taskExecutor) {
+    public TransactionService(ExecutorService taskExecutor, TransactionRepository repository) {
+        this.repository = repository;
         this.taskExecutor = taskExecutor;
     }
 
@@ -23,12 +28,21 @@ public class TransactionService {
         taskExecutor.submit(() ->{
             System.out.println("Processing " + txn.id() + " On Thread "+Thread.currentThread());
             try{
-                Thread.sleep(1000);
+                Thread.sleep(500);
+                // Main Change: Ab hum data save kar rahe hain
+                repository.save(txn);
                 System.out.println("Completed " + txn.id());
             }catch (InterruptedException e){
                 Thread.currentThread().interrupt();
             }
         });
+
+        repository.save(txn);
         return txn;
     }
+
+    public List<Transaction> getAllTransactions(){
+        return repository.findAll();
+    }
+
 }
