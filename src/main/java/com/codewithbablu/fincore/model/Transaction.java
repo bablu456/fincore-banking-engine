19844f86f2ef4f6249d182297dcfbdc8aa296341
bablu kumar <bablu707041dk@gmail.com>
2026-01-18 17:33:11 +0530

    refactor: convert Transaction record to JPA Entity

--- src/main/java/com/codewithbablu/fincore/model/Transaction.java
+++ src/main/java/com/codewithbablu/fincore/model/Transaction.java
@@ -1,22 +1,67 @@
 package com.codewithbablu.fincore.model;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
+import java.time.LocalDateTime;
 // 'record' automatically creates constructor, getters, and toString. Clean!
-public record Transaction(
-        String id,
-        double amount,
-        String type, // "CREDIT" or "DEBIT"
-        LocalDateTime timestamp,
-        TransactionStatus status
-) {
+public class Transaction {
+      private   String id;
+      private   double amount;
+      private   String type;// "CREDIT" or "DEBIT"
+      private   LocalDateTime timestamp;
+
+        @Enumerated(EnumType.STRING)
+        TransactionStatus status;
+
+        public Transaction(){}
+
     // Custom constructor to auto-generate ID and Time
-    public Transaction( double amount, String type) {
-        this(UUID.randomUUID().toString(), amount, type, LocalDateTime.now(),TransactionStatus.PENDING);
+    public Transaction(String id, double amount, String type, LocalDateTime timestamp, TransactionStatus status) {
+        this.id = id;
+        this.amount = amount;
+        this.type = type;
+        this.timestamp = timestamp;
+        this.status = status;
     }
 
-    public Transaction withStatus(TransactionStatus newStatus){
-        return new Transaction(id,amount,type,timestamp,newStatus);
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TransactionStatus status) {
+        this.status = status;
     }
 }
