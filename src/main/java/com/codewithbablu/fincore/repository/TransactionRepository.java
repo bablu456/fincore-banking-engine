02ba63490dadcaa74fd934c179613df4cf6138b3
bablu kumar <bablu707041dk@gmail.com>
2026-01-19 20:51:32 +0530

    feat: add custom JPQL queries for analytics

--- src/main/java/com/codewithbablu/fincore/repository/TransactionRepository.java
+++ src/main/java/com/codewithbablu/fincore/repository/TransactionRepository.java
@@ -4,6 +4,7 @@ package com.codewithbablu.fincore.repository;
 import com.codewithbablu.fincore.model.Transaction;
 import com.codewithbablu.fincore.model.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -16,5 +17,11 @@ import java.util.stream.Collectors;
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
 
     List<Transaction> findByStatus(TransactionStatus status);
+
+    @Query("SELECT SUM(t.amount) FROM Transaction t WHERE t.status = 'SUCCESS'")
+    double selectTotalSuccessAmount();
+
+    @Query("SELECT SUM(t.amount) FROM Transaction t WHERE t.status = 'FAILED'")
+    double countFailedTransaction();
 
 }
