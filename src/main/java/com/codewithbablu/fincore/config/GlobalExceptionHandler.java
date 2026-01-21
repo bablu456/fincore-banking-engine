607a8e760c6426a789df1fe5aeb24882364d93ad
bablu kumar <bablu707041dk@gmail.com>
2026-01-21 20:09:59 +0530

    feat: integrate ApiResponse with GlobalExceptionHandler

--- src/main/java/com/codewithbablu/fincore/config/GlobalExceptionHandler.java
+++ src/main/java/com/codewithbablu/fincore/config/GlobalExceptionHandler.java
@@ -1,25 +1,23 @@
 package com.codewithbablu.fincore.config;
 
+import com.codewithbablu.fincore.common.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 import java.util.Map;
-import java.util.Objects;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, Object>> handleBadInput(IllegalArgumentException e){
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of(
-                        "error",e.getMessage(),
-                        "timestamp",LocalDateTime.now(),
-                        "status", 400
-                ));
+    public ResponseEntity<ApiResponse<void>> handleValidationErrors(MethodArgumentNotValidException ex){
 
+        String errorMsg = ex.getBindingResult().getFieldError().getDefaultMessage();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("Something went wrong: "+ex.getMessage()));
     }
  }
