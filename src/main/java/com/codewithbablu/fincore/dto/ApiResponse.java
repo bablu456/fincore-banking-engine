28b24fb0228c7392e42f003e3bac77cd876a61ed
bablu kumar <bablu707041dk@gmail.com>
2026-01-25 16:59:12 +0530

    feat: implement global exception handling and improved api response

---
+++ src/main/java/com/codewithbablu/fincore/dto/ApiResponse.java
@@ -0,0 +1,36 @@
+package com.codewithbablu.fincore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T>{
+
+    private boolean success;
+    private String message;
+    private T data;
+    private String timestamp;
+
+    public ApiResponse(boolean success, String message, T data){
+        this.success = success;
+        this.message = message;
+        this.data = data;
+        this.timestamp = LocalDateTime.now().toString();
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data){
+        return new ApiResponse<>(true, message, data);
+    }
+    public static <T> ApiResponse<T> error(String message){
+        return new ApiResponse<>(false, message, null);
+    }
+
+    public static <T> ApiResponse<T> error (String message, T data){
+        return new ApiResponse<>(false, message, data);
+    }
+}
